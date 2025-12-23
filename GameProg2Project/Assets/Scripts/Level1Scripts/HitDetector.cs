1cff7418f833d1dcb18f9ede2638014e8f8a4537
jaimejoe <123969561+jaimejoe@users.noreply.github.com>
2025-12-23 17:34:12 -0500

    Fixed HitDetector

--- GameProg2Project/Assets/Scripts/Level1Scripts/HitDetector.cs
+++ GameProg2Project/Assets/Scripts/Level1Scripts/HitDetector.cs
@@ -2,28 +2,41 @@ using UnityEngine;
 
 public class HitDetector : MonoBehaviour
 {
+    public int damage = 20;
+    public AudioClip hitSound;
+    public float hitVolume = 0.5f; 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
-            EnemyController enemyController = other.GetComponent<EnemyController>();
+            if (hitSound != null)
+            {
+                AudioSource.PlayClipAtPoint(hitSound, other.transform.position, hitVolume);
+            }
+
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+
+            AIController enemyController = other.GetComponent<AIController>();
             if (enemyController != null)
             {
-                {
-                    enemyController.GotHit();
-                    Debug.Log("Calling GotHit()");
-                }
+                enemyController.GotHit();
             }
+
+            Debug.Log($"Hit enemy: {other.name}");
         }
+
         if (other.CompareTag("Player"))
         {
             PlayerController playerController = other.GetComponent<PlayerController>();
             if (playerController != null)
             {
-                {
-                    playerController.GotHit();
-                    Debug.Log("Calling GotHit()");
-                }
+                playerController.GotHit();
+                Debug.Log("Player hit!");
             }
         }
     }
