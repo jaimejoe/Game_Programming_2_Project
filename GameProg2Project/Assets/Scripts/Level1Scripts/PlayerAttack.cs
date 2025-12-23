052cd2877204c1519bd0eaea733d3ab010605f01
jaimejoe <123969561+jaimejoe@users.noreply.github.com>
2025-12-23 17:43:20 -0500

    fix

--- GameProg2Project/Assets/Scripts/Level1Scripts/PlayerAttack.cs
+++ GameProg2Project/Assets/Scripts/Level1Scripts/PlayerAttack.cs
@@ -1,30 +1,51 @@
-﻿using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody rb;
+    private PlayerController playerController;
+
     public float maxComboDelay = 1f;
-    public float cooldownTime = 1f;
     private int noOfClicks = 0;
     private float lastClickedTime = 0f;
-    private float cooldownEnd = 0f;
+    private float comboCooldownEnd = 0f;
     private bool comboContinue = false;
+    private float cooldownTime = 1f;
 
-    public float dashingTime = 5f;       
-    public float dashingPower = 20f;         
-    public float dashingCooldown = 1f;
+    public float dashingTime = 5f;
+    public float dashingPower = 20f;
+    public float dashingCooldown = 10f;
     private bool canDash = true;
     private bool isDashing = false;
+    private float dashCooldownEndTime = 0f;
+
+    private float attackEndTimer = 0f;
+    private bool waitingForCombo = false;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        playerController = GetComponent<PlayerController>();
     }
 
     void Update()
     {
+        if (!canDash && Time.time >= dashCooldownEndTime)
+        {
+            canDash = true;
+        }
+
+       
+        if (waitingForCombo && Time.time >= attackEndTimer)
+        {
+            playerController.EndAttack();
+            waitingForCombo = false;
+            noOfClicks = 0;
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && !isDashing)
         {
             StartCoroutine(Dash());
@@ -34,9 +55,14 @@ public class PlayerAttack : MonoBehaviour
         {
             Combo();
         }
+
         if (Time.time - lastClickedTime > maxComboDelay)
         {
             noOfClicks = 0;
+            if (playerController.IsAttacking)
+            {
+                playerController.EndAttack();
+            }
         }
     }
 
@@ -44,7 +70,7 @@ public class PlayerAttack : MonoBehaviour
     {
         lastClickedTime = Time.time;
 
-        if (Time.time < cooldownEnd)
+        if (Time.time < comboCooldownEnd)
             return;
 
         if (noOfClicks == 0)
@@ -52,6 +78,11 @@ public class PlayerAttack : MonoBehaviour
             noOfClicks = 1;
             comboContinue = false;
             anim.Play("Punch1", 0, 0f);
+            playerController.StartAttack();
+
+            
+            waitingForCombo = true;
+            attackEndTimer = Time.time + maxComboDelay;
             return;
         }
 
@@ -61,20 +92,40 @@ public class PlayerAttack : MonoBehaviour
         comboContinue = false;
         noOfClicks++;
 
+        
+        waitingForCombo = true;
+        attackEndTimer = Time.time + maxComboDelay;
+
         if (noOfClicks == 2)
         {
             anim.Play("Punch2", 0, 0f);
+            playerController.StartAttack();
         }
         else if (noOfClicks == 3)
         {
             anim.Play("FlyingKick", 0, 0f);
+            playerController.StartAttack();
         }
 
         if (noOfClicks >= 3)
         {
             noOfClicks = 0;
-            cooldownEnd = Time.time + cooldownTime;
+            comboCooldownEnd = Time.time + cooldownTime;
         }
+    }
+    public void OnAttackAnimationEnd()
+    {
+        if (!waitingForCombo)
+        {
+            playerController.EndAttack();
+        }
+    }
+
+    public void OnFlyingKickEnd()
+    {
+        playerController.EndAttack();
+        waitingForCombo = false;
+        noOfClicks = 0;
     }
 
     public void ComboWindow()
@@ -82,12 +133,13 @@ public class PlayerAttack : MonoBehaviour
         comboContinue = true;
     }
 
-
     private IEnumerator Dash()
     {
         canDash = false;
         isDashing = true;
+        playerController.StartDash();
 
+        dashCooldownEndTime = Time.time + dashingCooldown;
 
         anim.Play("FlyingKick", 0, 0f);
 
@@ -104,12 +156,9 @@ public class PlayerAttack : MonoBehaviour
             yield return new WaitForFixedUpdate();
         }
 
-
         rb.MovePosition(targetPos);
 
         isDashing = false;
-
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
+        playerController.EndDash();
     }
 }
