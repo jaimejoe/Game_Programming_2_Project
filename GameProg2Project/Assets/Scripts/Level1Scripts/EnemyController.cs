9348de193518755d411424f022bba6f8db26ecc3
Noice04 <NoiceD04@gmail.com>
2025-12-24 06:17:55 -0500

    final REAL FINITO ACCOMPLISHED
    (to be continued?)

---
+++ GameProg2Project/Assets/Scripts/Level1Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator animator;
+    private EnemyHealth enemyHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void GotHit()
+    {
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(20);
+            animator.SetTrigger("GotHit");
+        }
+    }
+}
